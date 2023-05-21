b3d1702ff9bd645cd9f1dca0a2675f10d9542be0
aleksandregvrm <aleksandregvrm@gmail.com>
2023-05-21 12:40:26 +0400

    starter commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Auth0Provider
+    domain="dev-0vjbsnzofylivo8m.us.auth0.com"
+    clientId="Iun9zhMVyLj9D4FtaWsGFjeUVrLWRjY5"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
+);
---
+++ src/pages/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+// will remove later
+// import { useUserContext } from "../context/user_context";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const PrivateRoute = ({ schildren, ...rest }) => {
+  const { user } = useAuth0();
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return user ? children : <Redirect to="/"></Redirect>;
+      }}
+    ></Route>
+  );
+};
+export default PrivateRoute;
